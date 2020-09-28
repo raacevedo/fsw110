25976196184d21185e15d330a5d678b63417cc90
“raacevedo“ <ralph.acevedo@bryanuniversity.edu>
2020-09-27 22:52:34 -0400

    selecting elements

--- week1/selecting_elements/index.js
+++ week1/selecting_elements/index.js
@@ -1,4 +1,4 @@
-//let h1Node = document.createElement('h1');
-//h1Node.id = 'h1';
-//h1Node.textContent = 'Please work!!!';
-//document.body.appendChild(h1Node);
+let h1Node = document.createElement('h1');
+h1Node.id = 'h1';
+h1Node.textContent = 'Please work!!!';
+document.body.appendChild(h1Node);
