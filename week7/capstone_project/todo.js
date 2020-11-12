9c86bb583fef775ca2fb124983b90b62f5885e9f
“raacevedo“ <ralph.acevedo@bryanuniversity.edu>
2020-11-12 01:43:00 -0500

    capstone

--- week7/capstone_project/todo.js
+++ week7/capstone_project/todo.js
@@ -12,16 +12,6 @@ function display(event) {
     div.id = document.body.children.length
     document.body.append(div)
 
-    // let button = document.createElement("button")
-    // button.innerHTML = "Delete"
-    // button.className = (document.body.children.length-1) +""
-    // div.append(button)
-
-    // button.addEventListener("click",(event)=>{
-    //     console.log(document.getElementById(event.target.className+""))
-    //     document.getElementById(event.target.className+"").remove()
-    // })
-
     div.innerHTML+=("<h3>Thank You</h3>");
     div.innerHTML+=("<h3>Here is your info:</h3>");
     div.innerHTML+=(name + "<br/>");
@@ -29,7 +19,30 @@ function display(event) {
     div.innerHTML+=(email + "<br/>");
     div.innerHTML+=(gender + "<br/>");
     div.innerHTML+=(type + "<br/>");
-    
+       
+   
+    function deleting (event){
+        
+        console.log("Trying to Delete")
+        console.log(document.getElementById(event.target.className+""))
+        document.getElementById(event.target.className).remove()
+   
+}
+let button = document.createElement("button")
+button.innerHTML = "Delete"
+button.className = (document.body.children.length-1) +""
+div.append(button)
+console.log(document.getElementsByClassName((document.body.children.length-1) +"")[0])
+
+// document.getElementsByClassName((document.body.children.length-1) +"")[0]
+// button.addEventListener("click", deleting)
+button.addEventListener("click", deleting); 
+document.getElementById("name").value = "";
+document.getElementById("age").value = "";
+document.getElementById("email").value = "";    
+document.getElementById("gender").value = ""; 
+document.getElementById("type").value = "";
+   
 }
 
 console.log(document.form)
